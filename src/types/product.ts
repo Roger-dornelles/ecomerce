f0de689d069255f2016ea554f092dc151a23b658
Roger <rsd.santo@outlook.com>
2023-04-05 05:31:01 -0300

    ++ feat: creation of product display layout ++ ATV-3

---
+++ src/types/product.ts
@@ -0,0 +1,12 @@
+export type ProductType = {
+  id: number;
+  name: string;
+  description: string;
+  createdAt: string;
+  isInstallments: true;
+  quantity: number;
+  updatedAt: string;
+  userID: number;
+  value: string;
+  photosID: number[] | number;
+};
